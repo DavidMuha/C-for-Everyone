b1fea2da584128445703632f2a01748f2128efb3
DavidMuha <155854049+DavidMuha@users.noreply.github.com>
2025-02-06 08:31:05 +0100

    Week_1_1st_Assignment_David_Muha_.c

---
+++ Week-1_1st_Assignment_David_Muha.c
@@ -0,0 +1,20 @@
+// The following program is supposed to write hello world onto the screen but it has syntax errors - find and correct.
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    printf(‘ hello world\n’);
+    return 0;
+}
+*/
+
+// my correction
+
+#include <stdio.h>
+int main(void)
+{
+    // "" instead of ‘’
+    printf(" hello world\n");
+    return 0;
+}
