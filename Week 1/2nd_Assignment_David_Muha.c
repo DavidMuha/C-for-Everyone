06b4bc9fd5795c00c4a9ada98ed51b6d4eda5876
DavidMuha <155854049+DavidMuha@users.noreply.github.com>
2025-02-06 08:46:04 +0100

    Create 2nd_Assignment_David_Muha.c

---
+++ Week 1/2nd_Assignment_David_Muha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+    2nd Assignment (Week 1): Print a poem
+    David Andrei Muha
+    19.12.2024
+*/
+
+int main()
+{
+    printf("\nI really hate this damned machine,\nI wish that they would sell it.\nIt never does quite what I want,\nbut only what I tell it.\n");
+    return 0;
+}
